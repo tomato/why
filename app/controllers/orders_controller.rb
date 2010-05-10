bc3e08bb35b16cb83afa572b51f7dd8b04f134f8
Tom Howlett <tom@Tom-Howletts-MacBook-Pro.local>
2010-05-10 21:55:17 +0100

    create order function created

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,4 +7,11 @@ class OrdersController < ApplicationController
     @products = Product.find_all_by_supplier_id(@supplier)
     @product_categories = @products.group_by { |p| p.category }
   end
+
+  def create
+    render :update do |page|
+      page << "alert('We updated your order')"
+    end
+  end
+
 end

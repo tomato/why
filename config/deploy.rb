c81d9fe1c7c1eed1ea21a52dd40d09b71ec09dc6
Tom Howlett <tom@Tom-Howletts-MacBook-Pro.local>
2010-09-07 20:42:03 +0100

    added sudo to bundle install

--- config/deploy.rb
+++ config/deploy.rb
@@ -157,7 +157,7 @@ namespace :bundler do
  
   task :bundle_new_release, :roles => :app do
     bundler.create_symlink
-    run "cd #{release_path} && bundle install --without test"
+    run "cd #{release_path} && sudo bundle install --without test"
   end
 end
  
